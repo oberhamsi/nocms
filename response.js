84e89bd3b6de3c530e76ea4651c4b60951265519
Simon Oberhammer <simon@nekapuzer.at>
2011-03-09 21:50:21 +0100

    add badrequest, minor cleanup

--- response.js
+++ response.js
@@ -20,18 +20,26 @@ function respond(body) {
    };
 };
 
-function error(body) {
+function error() {
    return {
       status: 200, // 500,
       headers: {'Content-Type': 'text/html'},
-      body: ['<h1>500</h1> <quote>You know what they say about me? I suck!</quote>']
+      body: ['<h1>500 Internal Server Error</h1> <quote>You know what they say about me? I suck!</quote>']
    }
 };
 
-function notfound(body) {
+function notfound() {
    return {
       status: 200, // 404,
       headers: {'Content-Type': 'text/html'},
-      body: ["<h1>404</h1> <quote>If we don't got it, you don't want it!</quote>"]
+      body: ["<h1>404 Not Found</h1> <quote>If we don't got it, you don't want it!</quote>"]
+   };
+};
+
+function badrequest() {
+   return {
+      status: 200, // 400 bad request
+      headers: {'content-Type': 'text/html'},
+      body: ["<h1>400 Bad request</h1> <quote>Die in a fire!</quote>"]
    };
 };

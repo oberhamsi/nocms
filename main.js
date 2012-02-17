fa64545a19bc38cbe913444716dcf361c149042a
Simon Oberhammer <simon@nekapuzer.at>
2012-02-17 15:01:21 +0100

    towards packaged app
    currently broken

--- main.js
+++ main.js
@@ -1,52 +1,22 @@
-var {Application} = require('stick');
-var log = require('ringo/logging').getLogger(module.id);
+var term = require("ringo/term");
+var {Parser} = require("ringo/args");
 
-var config = require('./config');
-var {error} = require('./response');
-var authMiddleware = require('./basicauth');
-var captcha = require('./captcha');
+function main(args) {
+    var cmd = args.shift() || "help";
+    var module = null;
+    try {
+        module = require("./lib/commands/" + cmd);
+    } catch (e) {
+        term.writeln(term.RED, "Unknown command '" + cmd +
+                "', use 'help' to get a list of available commands",
+                term.RESET);
+        return false;
+    }
+    module[cmd](args);
+    return;
+}
 
-var app = exports.app = Application();
-app.configure('session', authMiddleware, 'static', 'upload', 'params', captcha, 'mount');
-app.static(module.resolve('public'), 'static');
-// aut for paths containing ! and a secure action
-app.basicauth(/(.*)\!(edit|files)(.*)/, config.backend.username, config.backend.password_sha1);
-
-var actions = require('./actions');
-app.mount('/', function(request) {
-   var path = request.pathInfo;
-   request.isPost = request.method.toLowerCase() === 'post';
-
-   // path up to ! is page's path name. after ! comes action (optional).
-   var NOCMS_REGEX = new RegExp(/^\/([^\!]*)(?:\!(edit|comment|files))?/);
-   var matches = path.match(NOCMS_REGEX);
-   if (matches !== null) {
-      path = matches[1];
-      // remove trailing /
-      path = path.substring(path.length-1) === '/' ? path.substring(0, path.length-1) : path;
-      // if search engine request: return canonical html now
-      // @see http://www.google.com/support/webmasters/bin/answer.py?hl=en&answer=174992
-      if (request.params._escaped_fragment_ !== undefined) {
-         return actions.canonicalPage(request, path);
-      }
-
-      var action = null;
-      if (matches[2] && matches[2].length) {
-         action = actions[matches[2]];
-      } else {
-         action = actions.page;
-      }
-      try {
-         return action(request, path);
-      } catch(e) {
-         log.error(e);
-         return error();
-      }
-   }
-   return notfound;
-});
-
-// Script run from command line
-if (require.main === module) {
-    require('ringo/httpserver').main(module.id);
+if (require.main == module.id) {
+    // call main, stripping the module location from args
+    main(system.args.splice(1));
 }
